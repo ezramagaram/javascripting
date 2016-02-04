ebec74c475b6373f517492a050a69428186f7580
Ezra Magaram <ezramagaram@seattleacady.org>
2016-02-04 09:33:04 -0800

    complete if-statement.js challenge

--- if-statement.js
+++ if-statement.js
@@ -16,3 +16,6 @@
      javascripting verify if-statement.js
 */
 
+var fruit = 'orange';
+if (fruit.length > 5) {console.log("The fruit name has more than five characters.")}
+else console.log("this fruit has less than 5 charactors");
